27e44c6072f49edd08de5f771220496badea779a
HanseongBae <bha4388@naver.com>
2024-04-27 22:57:50 +0900

    init

---
+++ corssfit-admin-back/src/main/java/com/crossfit/admin/CorssfitAdminApplication.java
@@ -0,0 +1,13 @@
+package com.crossfit.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorssfitAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorssfitAdminApplication.class, args);
+	}
+
+}
---
+++ corssfit-admin-back/src/test/java/com/crossfit/admin/CorssfitAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crossfit.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorssfitAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
